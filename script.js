14dbadc809571012916380eeea642cf598d7fdb4
Alex102404 <123668276+Alex102404@users.noreply.github.com>
2023-05-04 10:18:10 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Declare your genre arrays here
+let zombier=["https://resizing.flixster.com/kJ4b906QdQHz__5Drl7fK8N1ytI=/206x305/v2/https://flxt.tmsimg.com/assets/p3946_p_v8_ad.jpg","https://resizing.flixster.com/6jHBgB_YrZ9RFKgR6hA13BbXedQ=/206x305/v2/https://flxt.tmsimg.com/assets/p9264983_p_v8_ab.jpg","https://m.media-amazon.com/images/M/MV5BMTU5MDg0NTQ1N15BMl5BanBnXkFtZTcwMjA4Mjg3Mg@@._V1_.jpg"];
+let horror=["https://m.media-amazon.com/images/M/MV5BMTY2ODc2NTQ2OF5BMl5BanBnXkFtZTYwNzA4OTU3._V1_.jpg","https://www.flickeringmyth.com/wp-content/uploads/2020/08/host_quadshudder_mr.jpg","https://is5ssl.mzstatic.com/image/thumb/U6cpeuFdzMIB2mX7GIltFA/1200x675mf.jpg"];
+
+let action=["https://m.media-amazon.com/images/M/MV5BZjRlNDUxZjAtOGQ4OC00OTNlLTgxNmQtYTBmMDgwZmNmNjkxXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg","https://m.media-amazon.com/images/M/MV5BOTgyMWQ0ZWUtN2Q2MS00NmY0LWI3OWMtNjFkMzZlNDZjNTk0XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg","https://www.thebroadtheater.com/wp-content/uploads/2023/03/john_wick_chapter_four.jpg"];
+// Make sure to declare your HTML elements as variables! 
+let result= document.querySelector(".Result");
+// Submit Button 
+let submitButton = document.querySelector(".submit"); 
+submitButton.onclick = function() {
+    
+};  
+ 
+let suggestButton=document.querySelector(".suggest");
